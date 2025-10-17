e8a1a3e0731d4d5988d9df42cfd2ce602452f5a7
MyeongSooJang <mmsgod0427@naver.com>
2025-10-17 21:55:54 +0900

    feat: 문자열 덧셈 계산기 전체 흐름 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,11 @@
 package calculator;
 
+import calculator.domain.Calculator;
 import calculator.domain.InputString;
+import calculator.domain.Numbers;
+import calculator.domain.Tokens;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,5 +13,9 @@ public class Application {
         InputView inputView = new InputView();
         String input = inputView.readInput();
         InputString inputString = new InputString(input);
+        Tokens tokens = new Tokens(inputString.getValue());
+        Numbers numbers = new Numbers(tokens.getValues());
+        Calculator calculator = new Calculator(numbers);
+        new OutputView().printResult(calculator.calculate());
     }
 }
