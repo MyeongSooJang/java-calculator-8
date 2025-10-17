e1b6a4c28c97d69be5070b13e9c9656c954b66bc
MyeongSooJang <mmsgod0427@naver.com>
2025-10-17 17:29:54 +0900

    test: 빈 문자열 입력 시 빈 토큰 반환 테스트 추가

---
+++ src/test/java/calculator/domain/TokensTest.java
@@ -0,0 +1,19 @@
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TokensTest {
+    @Test
+    @DisplayName("빈 문자열이 들어오면 빈 리스트를 반환한다.")
+    void emptyStringReturnsEmptyList() {
+        String input = "";
+
+        Tokens tokens = new Tokens(input);
+
+        assertThat(tokens).isEmpty();
+    }
+
+}
