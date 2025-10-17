179acf393b23e2a4eff3a3574301c39c5e41f2e9
MyeongSooJang <mmsgod0427@naver.com>
2025-10-17 21:02:18 +0900

    test: 음수, 숫자가 아닌 문자열 입력 시 예외 발생 테스트 추가

--- src/test/java/calculator/domain/NumbersTest.java
+++ src/test/java/calculator/domain/NumbersTest.java
@@ -1,13 +1,27 @@
 package calculator.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class NumbersTest {
+
+    @Test
+    @DisplayName("나누어진 토큰을 숫자로 변환")
+    void parseSingleNumber() {
+        List<String> input = List.of("1");
+
+        Numbers numbers = new Numbers(input);
+
+        assertThat(numbers.getValues()).containsExactly(1);
+    }
+
     @Test
     @DisplayName("나누어진 토큰들을 숫자로 변환")
-    void parseNumbers() {
+    void parseMultipleNumbers() {
         List<String> input = List.of("1", "2", "3");
 
         Numbers numbers = new Numbers(input);
@@ -15,4 +29,23 @@ public class NumbersTest {
         assertThat(numbers.getValues()).containsExactly(1, 2, 3);
     }
 
+    @Test
+    @DisplayName("숫자가 아닌 값인 경우, 예외 발생")
+    void throwExceptionWhenInvalidNumbers() {
+        List<String> input = List.of("1", "2", "ㄴ");
+
+        assertThatThrownBy(() -> new Numbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자가 아닌 값이 포함되어 있습니다 : " + "ㄴ");
+    }
+
+    @Test
+    @DisplayName("숫자가 음수인 경우, 예외 발생")
+    void throwExceptionWhenNegativeNumbers() {
+        List<String> input = List.of("1", "2", "-2");
+
+        assertThatThrownBy(() -> new Numbers(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("음수는 입력이 불가능합니다.");
+    }
 }
