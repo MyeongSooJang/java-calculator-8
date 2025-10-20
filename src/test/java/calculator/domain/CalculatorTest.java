46dc7a4a5fac8695ee9b33bf8c1d25c1d3c717ad
MyeongSooJang <mmsgod0427@naver.com>
2025-10-20 14:42:59 +0900

    test: Calculator 구분자 테스트 케이스 추가

--- src/test/java/calculator/domain/CalculatorTest.java
+++ src/test/java/calculator/domain/CalculatorTest.java
@@ -2,7 +2,6 @@ package calculator.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,30 +10,60 @@ public class CalculatorTest {
     @Test
     @DisplayName("비어있는 경우 0을 반환한다.")
     void calculateEmpty() {
-        Numbers numbers = new Numbers(List.of());
+        String input = "";
 
-        Calculator calculator = new Calculator(numbers);
+        Calculator calculator = new Calculator();
 
-        assertThat(calculator.calculate()).isEqualTo(0);
+        assertThat(calculator.calculate(input)).isEqualTo(0);
     }
 
     @Test
     @DisplayName("단일 값의 경우 그대로 반환한다.")
     void calculateSingle() {
-        Numbers numbers = new Numbers(List.of("1"));
+        String input = "1";
 
-        Calculator calculator = new Calculator(numbers);
+        Calculator calculator = new Calculator();
 
-        assertThat(calculator.calculate()).isEqualTo(1);
+        assertThat(calculator.calculate(input)).isEqualTo(1);
     }
 
     @Test
-    @DisplayName("여러 값을 가지는 경우 모든 수를 더한 값을 반환한다.")
-    void calculateMultiple() {
-        Numbers numbers = new Numbers(List.of("1", "2", "3"));
+    @DisplayName("콤마를 구분자로 들어온 경우, 각각 숫자로 나누뒨 숫자들의 총합을 계산한다.")
+    void calculateMultipleWithComma() {
+        String input = "1,2,3";
 
-        Calculator calculator = new Calculator(numbers);
+        Calculator calculator = new Calculator();
 
-        assertThat(calculator.calculate()).isEqualTo(6);
+        assertThat(calculator.calculate(input)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("콜론을 구분자로 들어온 경우, 각각 숫자로 나누뒨 숫자들의 총합을 계산한다.")
+    void calculateMultipleWithColon() {
+        String input = "1:2:3";
+
+        Calculator calculator = new Calculator();
+
+        assertThat(calculator.calculate(input)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("쉼표와 콜론을 혼합하여 사용한 경우, 숫자들의 합을 계산한다.")
+    void calculateMultipleWithCommaAndColon() {
+        String input = "1,2:3";
+
+        Calculator calculator = new Calculator();
+
+        assertThat(calculator.calculate(input)).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 구분된 숫자들의 합을 계산한다.")
+    void calculateWithCustomDelimiter() {
+        String input = "//;\n1;2;3";
+
+        Calculator calculator = new Calculator();
+
+        assertThat(calculator.calculate(input)).isEqualTo(6);
     }
 }
